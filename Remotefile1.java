5da631dd90f4dac78c21ef793dee4f78c3684d48
Alpa <gargalpa009@gmail.com>
2022-03-23 22:00:41 -0400

    Revert "Merge branch 'Enhancement' of https://github.com/gargalpa009/WyniscoRepositroy2 into Enhancement"
    This reverts commit 13a28ebf87fb531903e6d85f8a80a90f528e982b, reversing
    changes made to d63b44b946b6a497f0b02ce37e917493362d5d4b.

--- Remotefile1.java
+++ Remotefile1.java
@@ -1,5 +1,4 @@
 created this line on 03-23-2022
 created this line on 03-23-2022-line2
 created this line on 03-23-2022-switch  to enahncement
-change done in local fil
-changes have been done in remote file
+change done in local file
